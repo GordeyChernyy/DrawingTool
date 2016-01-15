aea3f31cec47d9afd9d0d1639f5bc97d549f4b19
gordey <kewava@gmail.com>
2016-01-15 00:01:53 -0500

    clean DreamBrush: get rid of the swatch and colors

--- src/DreamBrush.cpp
+++ src/DreamBrush.cpp
@@ -10,35 +10,23 @@
 const string NAME = "Dream Brush";
 
 void DreamBrush::setup(){
-//    BrushBase::names.push_back("BrushBase");
     parameters.setName("DreamBrush");
     parameters.add(historyMax.set("historyMax", 2000, 100, 10000));
     parameters.add(brushType.set("type line", 0, 0, 1));
     parameters.add(size.set("size", 300, 0, 800));
     parameters.add(opacity.set("opacity", 20, 0, 255));
     parameters.add(lineWidth.set("lineWidth", 0.2, 0., 5.));
-    parameters.add(swatch.set("swatch", 0, 0, 12));
-    parameters.add(bgColor.set("bgColor", ofColor(0, 0), ofColor(0, 0), ofColor(255, 255)));
-    parameters.add(activeColor.set("activeColor", ofColor(0, 0), ofColor(0, 0), ofColor(255, 255)));
-    parameters.add(col1.set("col1", ofColor(0, 0), ofColor(0, 0), ofColor(255, 255)));
-    parameters.add(col2.set("col2", ofColor(0, 0), ofColor(0, 0), ofColor(255, 255)));
-    parameters.add(col3.set("col3", ofColor(0, 0), ofColor(0, 0), ofColor(255, 255)));
     canvas.allocate(ofGetWidth(), ofGetHeight());
-    canvas.begin(); ofClear(bgColor); canvas.end();
-    swatch.addListener(this,&DreamBrush::changeSwatch);
+    canvas.begin(); ofClear(0, 0); canvas.end();
     brushType.addListener(this,&DreamBrush::changeBrush);
 }
-void DreamBrush::drawToCanvas(float x, float y, float pressure){
-    canvas.begin();
-        drawBrush(x, y, pressure);
-    canvas.end();
-}
-void DreamBrush::updateCanvas(ofFbo *fbo, float x, float y, float pressure, ofColor col){
+
+void DreamBrush::updateCanvas(ofFbo *fbo, float x, float y, float pressure, ofColor color){
     fbo->begin();
-        drawBrush(x, y, pressure);
+        drawBrush(x, y, pressure, color);
     fbo->end();
 }
-void DreamBrush::drawBrush(float x, float y, float pressure){
+void DreamBrush::drawBrush(float x, float y, float pressure, ofColor color){
     glEnable(GL_BLEND);
     glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
     ofVec2f m;
@@ -50,9 +38,9 @@ void DreamBrush::drawBrush(float x, float y, float pressure){
         float joinchance = pressure/history.size() + m.distance(*h)/dist_points;
         if (joinchance < ofRandom(0.4)) {
             ofNoFill();
-            ofSetColor(activeColor, opacity);
+            ofSetColor(color, opacity);
             ofSetLineWidth(lineWidth);
-            ofLine(h->x, h->y, m.x, m.y);
+            ofDrawLine(h->x, h->y, m.x, m.y);
         }
     }
     glDisable(GL_BLEND);
@@ -74,26 +62,7 @@ void DreamBrush::onMouseUp(int x, int y){
 void DreamBrush::clearHistory(){
     history.clear();
 }
-void DreamBrush::clear(){
-    canvas.allocate(ofGetWidth(), ofGetHeight());
-    canvas.begin(); ofClear(bgColor); canvas.end();
-}
-void DreamBrush::changeColor(int num){
-    ofColor *colors = new ofColor[3];
-    colors[0] = col1;
-    colors[1] = col2;
-    colors[2] = col3;
-    if (num==3) {
-        activeColor = bgColor;
-    }else{
-        activeColor = colors[num];
-    }
-}
-void DreamBrush::changeSwatch(int &swatch){
-    col1 = swatches[swatch][0];
-    col2 = swatches[swatch][1];
-    col3 = swatches[swatch][2];
-}
+
 void DreamBrush::changeBrush(int &_brushType){
     switch (_brushType) {
         case 0:
@@ -108,10 +77,6 @@ void DreamBrush::changeBrush(int &_brushType){
     }
 }
 void DreamBrush::draw(){
-}
-void DreamBrush::resize(){
-    canvas.allocate(ofGetWidth(), ofGetHeight());
-    canvas.begin(); ofClear(255); canvas.end();
 }
 
 string DreamBrush::name(){
--- src/DreamBrush.h
+++ src/DreamBrush.h
@@ -21,12 +21,10 @@ public:
     void onMouseUp(int x, int y);
     void onMouseDown(int x, int y);
     void drawToCanvas(float x, float y, float pressure); // draw to fbo located in this class
-    void drawBrush(float x, float y, float pressure); // draw lines
+    void drawBrush(float x, float y, float pressure, ofColor color); // draw lines
     
     void updateCanvas(ofFbo *fbo, float x, float y, float pressure, ofColor col); // pass fbo
     void draw(); // draw fbo located in this class
-    void changeColor(int num);
-    void changeSwatch(int &swatch);
     void changeBrush(int &brushType);
     void resize();
     void begin();
@@ -39,93 +37,6 @@ public:
     ofParameter<int> historyMax;
     ofParameter<float> opacity;
     ofParameter<float> lineWidth;
-    ofParameter<int> swatch;
-    ofParameter<ofColor> bgColor;
-    ofParameter<ofColor> activeColor;
-    ofParameter<ofColor> col1;
-    ofParameter<ofColor> col2;
-    ofParameter<ofColor> col3;
    
     vector<ofVec2f> history;
-    
-    ofColor swatches[13][4] = {
-        {
-            ofColor(110,255,2),
-            ofColor(235,255,19),
-            ofColor(255,9,79),
-            ofColor(8,10,10)},
-        {
-            ofColor(211,253,253),
-            ofColor(197,255,48),
-            ofColor(231,83,49),
-            ofColor(214,9,44)
-        },
-        {
-            ofColor(254,5,12),
-            ofColor(138,4,109),
-            ofColor(254,129,253),
-            ofColor(8,10,11)
-        },
-        {
-            ofColor(244,255,143),
-            ofColor(173,9,70),
-            ofColor(242,250,1),
-            ofColor(189,199,219)
-        },
-        {
-            ofColor(57,150,254),
-            ofColor(147,83,255),
-            ofColor(254,26,14),
-            ofColor(243,255,133)
-        },
-        {
-            ofColor(250,23,30),
-            ofColor(29,136,82),
-            ofColor(243,250,39),
-            ofColor(235,145,24)
-        },
-        {
-            ofColor(184,145,252),
-            ofColor(195,64,235),
-            ofColor(240,145,33),
-            ofColor(212,171,138)
-        },
-        {
-            ofColor(85,193,144),
-            ofColor(240,255,92),
-            ofColor(187,89,68),
-            ofColor(193,188,140)
-        },
-        {
-            ofColor(234,255,10),
-            ofColor(254,3,15),
-            ofColor(254,184,224),
-            ofColor(253,178,94)
-        },
-        {
-            ofColor(240,255,11),
-            ofColor(167,255,2),
-            ofColor(193,147,254),
-            ofColor(246,1,11)
-        },
-        {
-            ofColor(236,0,22),
-            ofColor(227,61,241),
-            ofColor(219,128,190),
-            ofColor(208,120,254)
-        },
-        {
-            ofColor(206,206,194),
-            ofColor(254,3,66),
-            ofColor(119,62,255),
-            ofColor(168,80,57)
-        },
-        {
-            ofColor(87,202,140),
-            ofColor(236,255,24),
-            ofColor(173,236,148),
-            ofColor(194,61,176)
-        },
-        
-    };
 };
